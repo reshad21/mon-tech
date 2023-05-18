73617546eda35416f9c2f16b052d111f99bc4c02
reshad21 <miakala7@gmail.com>
2023-05-18 22:26:52 +0600

    axios install successfully

--- src/features/products/productSlice.js
+++ src/features/products/productSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchProducts } from "./productsAPI";
 
 const initialState = {
     products: [],
@@ -8,10 +9,12 @@ const initialState = {
 }
 
 export const getProduct = createAsyncThunk("products,getProduct", async () => {
-    const res = await fetch("http://localhost:5000/products");
-    const data = await res.json();
-    // console.log(data);
-    return data;
+    // const res = await fetch("http://localhost:5000/products");
+    // const data = await res.json();
+    // return data;
+
+    const products = fetchProducts();
+    return products;
 
 })
 
@@ -23,13 +26,11 @@ const productSlice = createSlice({
             .addCase(getProduct.pending, (state, action) => {
                 state.isLoading = true;
                 state.isError = false;
-
             })
             .addCase(getProduct.fulfilled, (state, action) => {
                 state.products = action.payload;
                 state.isLoading = false;
                 state.isError = false;
-
             })
             .addCase(getProduct.rejected, (state, action) => {
                 state.products = [];
@@ -37,7 +38,6 @@ const productSlice = createSlice({
                 state.isError = true;
                 state.error = action.error.message;
             })
-
     }
 })
 
---
+++ src/features/products/productsAPI.js
@@ -0,0 +1,5 @@
+export const fetchProducts = async () => {
+    const res = await fetch("http://localhost:5000/products");
+    const data = await res.json();
+    return data;
+}
